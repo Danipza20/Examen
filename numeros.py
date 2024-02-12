de33fd8728bad21fd1187a52d176a257dbd7e59c
Danipza20 <159832111+Danipza20@users.noreply.github.com>
2024-02-12 15:02:15 -0600

    Examen_parcial

---
+++ numeros.py
@@ -0,0 +1,4 @@
+numero1= input ("ingresar el primer numero")
+numero2 = input ("ingresar el segundo numero")
+
+print = "La suma de los dos numeros es", (numero1+numero2)
---
+++ traingul.py
@@ -0,0 +1,4 @@
+base = input ("favor ingresar la base")
+altura = input("favor ingresar la altura")
+
+print = "El area del triangulo es", base * altura /2 
